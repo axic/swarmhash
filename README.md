3b7f6a01c42acad85aeb05d0bf3bd29d1e4c2c9e
Alex Beregszaszi <alex@rtfs.hu>
2016-12-01 13:31:10 +0000

    Initial version

---
+++ README.md
@@ -0,0 +1,20 @@
+# swarmhash
+
+This library can be used to calculate Ethereum Swarm hashes of data blobs.
+
+## Usage
+
+The only input it supports is a `Buffer` and will return a `Buffer` containing the 256 bit hash.
+
+Example:
+```js
+const swarmhash = require('swarmhash')
+
+swarmhash(new Buffer('Hello World')).toString('hex') // d85117d40c1b74239bf0b0c4f8201e2be7d85c36efbbddc77fb9b58ed3964287
+```
+
+## License
+
+MIT License
+
+Copyright (C) 2016 Alex Beregszaszi
---
+++ index.js
@@ -0,0 +1,33 @@
+const Keccak = require('keccakjs')
+
+// NOTE: it only supports 48 bit lengths
+function swarmHashBlock (data, totalLength) {
+  var hash = new Keccak(256)
+  var tmp = Buffer.alloc(8)
+  tmp.writeUIntLE(totalLength, 0, 6)
+  hash.update(tmp)
+  hash.update(data)
+  return new Buffer(hash.digest('bin'), 'binary')
+}
+
+function swarmHash (data) {
+  const length = data.length
+
+  if (length <= 4096) {
+    return swarmHashBlock(data, length)
+  }
+
+  var maxSize = 4096
+  while ((maxSize * (4096 / 32)) < length) {
+    maxSize *= (4096 / 32)
+  }
+
+  var innerNodes = []
+  for (var i = 0; i < length; i += maxSize) {
+    const size = (maxSize < (length - i)) ? maxSize : (length - i)
+    innerNodes.push(swarmHash(data.slice(i, i + size)))
+  }
+  return swarmHashBlock(Buffer.concat(innerNodes), length)
+}
+
+module.exports = swarmHash
