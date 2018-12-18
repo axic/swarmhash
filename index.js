52679b0b8673cfac7110defd5f0f1336fb4e1cbc
Alex Beregszaszi <alex@rtfs.hu>
2018-12-18 19:59:52 +0000

    aaa

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const assert = require('assert')
 const Buffer = require('safe-buffer').Buffer
 const Keccak = require('keccakjs')
 
@@ -31,13 +32,61 @@ function swarmHash (data) {
   return swarmHashBlock(Buffer.concat(innerNodes), length)
 }
 
+function keccak256Buffer (data) {
+  var hash = new Keccak(256)
+  hash.update(data)
+  return Buffer.from(hash.digest('bin'), 'binary')
+}
+
+// Assumes `data` is a Buffer.
+function pyramidHashSection (data, sectionLength) {
+  var section
+  if (data.length === sectionLength) {
+    section = data
+  } else {
+    const length = data.length / 2
+    section = Buffer.concat([
+      pyramidHashSection(data.slice(0, length), length),
+      pyramidHashSection(data.slice(length), length)
+    ])
+//    section = Buffer.concat([ keccak256Buffer(data.slice(0, length)), keccak256Buffer(data.slice(length)) ])
+  }
+  console.log(section)
+
+  var hash = new Keccak(256)
+  hash.update(section)
+  return Buffer.from(hash.digest('bin'), 'binary')
+}
+
+// NOTE: this is currently hardcoded to use keccak256
 function pyramidHash (data) {
-  throw new Error('not implemented')
-  return Buffer.from(0)
+  const chunkSize = 4096
+  const hashSize = 32
+  const sectionLength = 2 * hashSize
+  var maxDataLength = 2
+  const segmentCount = Math.ceil(chunkSize / hashSize)
+  while (maxDataLength < segmentCount) maxDataLength *= 2
+  maxDataLength *= hashSize
+
+  console.log(chunkSize, segmentCount, hashSize, sectionLength, maxDataLength)
+
+  assert(Buffer.isBuffer(data))
+
+  // Need to zero pad if data is too short.
+  if (data.length < maxDataLength) {
+    data = Buffer.concat([
+      data,
+      Buffer.alloc(maxDataLength - data.length)
+    ])
+  }
+  
+  console.log(data.length)
+
+  return pyramidHashSection(data, maxDataLength)
 }
 
 module.exports = function (opts) {
-  opts = opts || {}
+  opts = opts || { mode: 'poc3' }
   if (opts.mode === 'poc2') {
     return swarmHash
   } else {
